36055121e11b13b529d0976cdabb6014a24c4644
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2018-12-03 21:53:34 +0100

    Add string representation to layouts model

--- content_generator/layouts/models.py
+++ content_generator/layouts/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Layout(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return 'layout {id}: {name}'.format(id=self.id, name=self.name)
